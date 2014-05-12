ad9e151a95c0d4806fbe65a910a194a3546535d1
bhomnath <bhomnath@outlook.com>
2014-05-12 12:14:20 +0545

    working on home page view

--- application/views/templates/content.php
+++ application/views/templates/content.php
@@ -20,7 +20,7 @@
                             </div>
                             
                             <div class='contentContainerFooterLeft'></div>
-                            <div class="redColouredDiv" id='contentContainerFooterRight'> </div>
+                            <div class="redColouredDiv" id='contentContainerFooterRight'><p>Details</p></div>
                            
                         </div>
                         
@@ -37,13 +37,17 @@
                             <div id="sideBarImage"><img src="<?php echo base_url() . "contents/images/shopping-cart-icon-614x460.png"; ?>"/> </div>   
                            <h3>Shopping Cart</h3>
                         </div>
+                         <?php for($i=0; $i<2; $i++)
+                        {?>
                         <div class='sidebarContentNext'></div>
-                        <div class='sidebarContentNext'></div>
-                        <div class='sidebarContentNext'></div>
-                        <div class='sidebarContentNext'></div>
-                        <div class='sidebarContentNext'></div>
-                        
+                      
+                        <?php } ?>
                         <div class="redColouredDiv" id='sidebarContent'><h3>Popular Posts</h3></div>
+                        <?php for($i=0; $i<4; $i++)
+                        {?>
+                        <div class='sidebarContentNext'></div>
+                      
+                        <?php } ?>
 
                     </div>  
  <div class="clear"> </div>
