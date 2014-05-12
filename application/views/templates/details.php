a70779f760b51cc07f2b850588405e85a9950f86
bhomnath <bhomnath@outlook.com>
2014-05-12 13:35:46 +0545

    details view is almost ready

--- application/views/templates/details.php
+++ application/views/templates/details.php
@@ -3,14 +3,28 @@
     <div id='contentWrapper'>
         <div class='contentHeaderDetails'>
             <p>Current location</p>
-            </div>
-        <div class="clear"></div>
+        </div>
+        
         <div id='content'>
             
             
-            <div class='contentContainerDetails'>
-                            
+        <div class='contentContainerDetails'>
+       <?php $data=array("image"=>'image', "title"=>'This is title', "details"=>'The details of tyhe clothing goes here', "price"=>'RS. 500/-'); ?>   
+            <div id='detailsImage'>
+                             <img src="<?php echo base_url() . "content/images/raincoat.png"; ?>"/>   
+            </div>  
+            
+            <div id="detailsDetail">
+                <p> <?php echo $data['details']; ?> </p> 
+                
             </div>
+            
+            <div class='contentContainerFooterLeft'>
+                <h4> Price: <?php echo $data['price']; ?></h4>
+            </div>
+            <div class="redColouredDiv" id='contentContainerFooterRight'><p>Buy Now</p></div>
+            
+        </div>
 
         </div>
 
