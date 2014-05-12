5da730ac2dadabd8a097065686e5e367a5faa419
bhomnath <bhomnath@outlook.com>
2014-05-12 12:33:51 +0545

    contents folder renamed

--- application/views/templates/content.php
+++ application/views/templates/content.php
@@ -16,11 +16,11 @@
                         <div class='contentContainerBox'>
                             <div class='contentContainerHeader'><?php echo $product->name; ?></div>
                             <div class='contentContainerImage'>
-                             <img src="<?php echo base_url() . "contents/images/raincoat.png"; ?>"/>   
+                             <img src="<?php echo base_url() . "content/images/raincoat.png"; ?>"/>   
                             </div>
                             
 
-                            <div class='contentContainerFooterLeft'>Details</div>
+                            <div class='contentContainerFooterLeft'><P>Details</p></div>
                             <div class='contentContainerFooterRight'><?php form_submit('action','Add to Cart');  ?></div>
 
                             <div class='contentContainerFooterLeft'></div>
