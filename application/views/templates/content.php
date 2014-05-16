700f2524e9d4ab9867788199f4367733a25479c0
sushilsth <sushilsth21@gmail.com>
2014-05-16 18:56:06 +0545

    resolve conflicts

--- application/views/templates/content.php
+++ application/views/templates/content.php
@@ -1,16 +1,12 @@
-
-
- <?php
+<?php
 $this->load->helper('currency');
 ?>
-
-
 <script>
     var base_url = <?php echo base_url(); ?>;
     $(document).ready(function() {
-       //adding item to the cart...
+        //adding item to the cart...
         $(".addToCart").click(function() {
-           $(this).parent().parent().parent().css({ opacity: 0.3 });
+            $(this).parent().parent().parent().css({opacity: 0.3});
             var id = $(this).val();
             var dataString = 'itemid=' + id;
             $.ajax({
@@ -19,16 +15,16 @@ $this->load->helper('currency');
                 data: dataString,
                 success: function(msgs)
                 {
-                   
+
                     $("#shopping_cart").html(msgs);
 
 
                 },
                 complete: function() {
-                  $(".contentContainerBox").css({  opacity: 1.0 });
+                    $(".contentContainerBox").css({opacity: 1.0});
                 }
             });
-            
+
         });
 
     });
@@ -36,60 +32,45 @@ $this->load->helper('currency');
 </script>
 
 
-<<<<<<< HEAD
-=======
+
+
+<div class='contentHeader'>
+    <h3>Recent products</h3>
+
+</div>
+<div id="itemContent">
+    <?php foreach ($product_info as $product) {
+        ?>
+        <div class='contentContainerBox'>
+
+            <div class='contentContainerHeader'><a href='<?php echo base_url() . "index.php/view/details/" . $product->id ?>'><h3><?php echo $product->name; ?></h3></div>
+            <div class='contentContainerImage'>
+                <img src="<?php echo base_url() . "content/images/" . $product->image1; ?>" alt="No images" height="150" width="130"/>   
+            </div></a>
+
+            <div class="contentContainerBottom"> 
+                <div class='contentContainerFooterLeft'><h4><?php get_currency($product->price); ?></h4></div>
+                <div class="redColouredDiv" class='contentContainerFooterRight'>
+
+                    <input type="button" value="<?php echo $product->id ?>" class="addToCart" id="addToCartBtn">  
 
 
 
->>>>>>> dec3ef139acb2170f501751c70cab6456832abf2
-          
-            
-            
-            <div class='contentHeader'>
-                 <h3>Recent products</h3>
-
+                </div>
             </div>
-            <div id="itemContent">
-            <?php foreach ($product_info as $product) {
-                
-                ?>
-                <div class='contentContainerBox'>
-
-                    <div class='contentContainerHeader'><a href='<?php echo base_url() . "index.php/view/details/". $product->id ?>'><h3><?php echo $product->name; ?></h3></div>
-                    <div class='contentContainerImage'>
-                        <img src="<?php echo base_url() . "content/images/".$product->image1; ?>" alt="No images" height="150" width="130"/>   
-                    </div></a>
-
-                        <div class="contentContainerBottom"> 
-                            <div class='contentContainerFooterLeft'><h4><?php get_currency($product->price); ?></h4></div>
-                    <div class="redColouredDiv" class='contentContainerFooterRight'>
-
-                        <input type="button" value="<?php echo $product->id ?>" class="addToCart" id="addToCartBtn">  
-                        
-                            
- 
-                        </div>
-                        </div>
-
-                    </div>
-            
-
-               
-            <?php } ?>
-            </div>
-
 
         </div>
 
-        
 
-        <!-- left side content closed here -->
-   
-        <div id='sidebar'>
-            <div class="redColouredDiv" id='sidebarContent'>
-                <div id="sideBarImage"><img src="<?php echo base_url() . "content/images/shopping-cart-icon-614x460.png"; ?>"/> </div>   
-                <h3>Shopping Cart</h3>
-            </div>
-                <div class='cartItems' id="shopping_cart">
-                    
-    
+
+    <?php } ?>
+</div>
+</div>
+<!-- left side content closed here -->
+
+<div id='sidebar'>
+    <div class="redColouredDiv" id='sidebarContent'>
+        <div id="sideBarImage"><img src="<?php echo base_url() . "content/images/shopping-cart-icon-614x460.png"; ?>"/> </div>   
+        <h3>Shopping Cart</h3>
+    </div>
+    <div class='cartItems' id="shopping_cart">
