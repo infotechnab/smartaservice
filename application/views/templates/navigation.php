edaa1236df45daf54a5c43591468194fe05960c9
bhomnath <bhomnath@outlook.com>
2014-05-19 16:36:54 +0545

    navigation managed from database

--- application/views/templates/navigation.php
+++ application/views/templates/navigation.php
@@ -1,15 +1,25 @@
 <div class="redColouredDiv" id="navigation">
     <div id="navigationLeft">
-                        <ul>
-                            <li><a href="<?php echo base_url().'index.php/view/index' ?>">HOME</a></li>
+        <div id="cssmenu">
+            <ul>
+                <?php
+
+            $this->load->helper('viewmenuhelper');
+
+            fetch_menu (query(0));
+
+        ?>
+                <li><a href="<?php echo base_url().'index.php/view/index' ?>">HOME</a></li>
                             <li><a href="#">ABOUT US</a></li>
-                            <li><a href="#">FEATURED</a></li>
-                            <li><a href="#">ACCESSORIES</a></li>
+                           
+                            
                             <li><a href="#">KID'S</a></li>
                             <li><a href="#">MEN'S</a></li>
                             <li><a href="#">WOMEN'S</a></li>
                             <li><a href="#">CONTACT US</a></li>
-                        </ul>
+            </ul>
+        </div>
+                       
     </div>
                             <div id='searchbox'>
 <input type="text" placeholder="Search" style="box-shadow: inset 0px  #888, inset 0px  #888; outline: none; border: 1px solid #dddddd; padding: 6px; margin: 0px;" required/>
