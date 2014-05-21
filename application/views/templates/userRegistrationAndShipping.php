013fe9404d7743ae1da585828ada58fc802f7649
bhomnath <bhomnath@outlook.com>
2014-05-21 13:09:57 +0545

    REGISTER

--- application/views/templates/userRegistrationAndShipping.php
+++ application/views/templates/userRegistrationAndShipping.php
@@ -1,26 +1,30 @@
 <script>
-var currentValue = 0;
-function handleClick(pickup) {
-    ('Old value: ' + currentValue);
-    ('New value: ' + pickup.value);
-    currentValue = pickup.value;
-    //alert(currentValue)
-                if(pickup.value==="shipDifferent"){
+    var currentValue = 0;
+    function handleClick(pickup) {
+        ('Old value: ' + currentValue);
+        ('New value: ' + pickup.value);
+        currentValue = pickup.value;
+        //alert(currentValue)
+        if (pickup.value === "shipDifferent") {
 
-           document.getElementById('RegisterRight').style.display = 'block';    
+            document.getElementById('RegisterRight').style.display = 'block';
 
-            }
-            else
-            {
-                document.getElementById('RegisterRight').style.display = 'none'; 
-            }
-}
+        }
+        else
+        {
+            document.getElementById('RegisterRight').style.display = 'none';
+        }
+    }
 </script>
 <div id="login">
     <div id="outerBorder">
         <div id="RegisterLeft">
 
             <table align="center" border="0" width="50%">
+               <tr>
+                    <td colspan="2"><h3 style="margin: 0px 0px 10px 0px; padding: 2px;">Personal Details</h3></td>
+                    
+                </tr>
                 <tr>
                     <td colspan="2"><p style="margin: 0px; padding: 2px;">User Name</p></td>
                 </tr>
@@ -49,19 +53,19 @@ function handleClick(pickup) {
                     <td><input type="text" name="District_address" placeholder="District/ State" size="20" style="outline: none; border: 1px solid #dddddd; padding: 10px; border-radius: 5px;" required/></td>
                     <td><input type="text" name="Country" placeholder="Country" size="20" style="outline: none; border: 1px solid #dddddd; padding: 10px; border-radius: 5px;" required/></td>
                 </tr>
-                 <tr>
+                <tr>
                     <td colspan="2"><p style="margin: 0px; padding: 2px;">Contact Number</p></td>
                 </tr>
                 <tr>
                     <td colspan="2"><input type="text" name="u_contact" placeholder="Contact Number" size="48" style="outline: none; border: 1px solid #dddddd; padding: 10px; border-radius: 5px;" required/></td>
                 </tr>
-                 <tr>
+                <tr>
                     <td colspan="2"><p style="margin: 0px; padding: 2px;">Email</p></td>
                 </tr>
                 <tr>
                     <td colspan="2"><input type="email" name="u_email" placeholder="Email" size="48" style="outline: none; border: 1px solid #dddddd; padding: 10px; border-radius: 5px;" required/></td>
                 </tr>
-                 <tr>
+                <tr>
                     <td colspan="2"><p style="margin: 0px; padding: 2px;">Password</p></td>
                 </tr>
                 <tr>
@@ -89,7 +93,10 @@ function handleClick(pickup) {
         <div id='RegisterRight'>
 
             <table align="center" border="0" width="50%">
-
+                <tr>
+                    <td colspan="2"><h3 style="margin: 0px 0px 10px 0px; padding: 2px;">Shipping Details</h3></td>
+                    
+                </tr>
                 <tr>
                     <td><p style="margin: 0px; padding: 2px;">First Name</p></td>
                     <td><p style="margin: 0px; padding: 2px;">Last Name</p></td>
@@ -124,9 +131,9 @@ function handleClick(pickup) {
                 <tr>
                     <td colspan="2"><input type="email" name="u_email" placeholder="Email" size="48" style="outline: none; border: 1px solid #dddddd; padding: 10px; border-radius: 5px;" required/></td>
                 </tr>
-               
 
-                
+
+
 
             </table>
 
