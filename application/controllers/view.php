7281d3693815728081ce2baa34d88f9b84374ee2
infotechnab <infotechnab@yahoo.com>
2014-05-26 15:27:08 +0545

    Now नेेपाालीी अल््सोो वोोर््क््स

--- application/controllers/view.php
+++ application/controllers/view.php
@@ -38,15 +38,16 @@ class View extends CI_Controller {
         $this->load->view('templates/footer');
         
       
-$db = mysql_connect('localhost','root','');
-if(!$db) echo "Cannot connect to the database – incorrect details";
-mysql_select_db('smartaservice'); $result=mysql_query('show tables');
-while($tables = mysql_fetch_array($result)) {
-foreach ($tables as $key => $value) {
-mysql_query("ALTER TABLE $value COLLATE utf8_general_ci");
-}}
-echo "The collation of your database has been successfully changed!";
+//$db = mysql_connect('localhost','root','');
+//if(!$db) echo "Cannot connect to the database – incorrect details";
+//mysql_select_db('smartaservice'); $result=mysql_query('show tables');
+//while($tables = mysql_fetch_array($result)) {
+//foreach ($tables as $key => $value) {
+//mysql_query("ALTER TABLE $value COLLATE utf8_general_ci");
+//}}
+//echo "The collation of your database has been successfully changed!";
 
+       
     }
 
  public function error(){
